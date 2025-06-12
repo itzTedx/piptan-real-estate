58c61e20086f32785af543d62bc49ac12fd774e2
melwin <114172287+itzTedx@users.noreply.github.com>
2025-06-12 18:10:34 +0400

    Add GSAP for animations in package.json and package-lock.json; update cSpell configuration to recognize 'gsap'; enhance page.tsx with new MomentumLines component and refined layout for improved user experience.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 import { ArrowTopRightIcon } from "@sanity/icons";
 
+import MomentumLines from "@/components/animation/momentum-lines";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
@@ -23,20 +25,20 @@ export default function Home() {
         </div>
         <Image src="/images/hero.webp" fill alt="" className="object-cover" />
       </section>
-      <section className="container grid grid-cols-12 gap-12 py-24">
-        <div className="col-span-5 space-y-3">
-          <p>Who we are?</p>
-          <h2 className="text-5xl font-bold">
+      <section className="relative container grid grid-cols-12 gap-12 py-24">
+        <div className="pointer-events-none z-10 col-span-5 space-y-3">
+          <p className="pointer-events-none">Who we are?</p>
+          <h2 className="pointer-events-none text-5xl font-bold">
             Your Real Estate{" "}
             <span className="text-primary-foreground">
               Partner in Every Step
             </span>
           </h2>
-          <Button variant="outline">
+          <Button variant="outline" className="pointer-events-auto w-fit">
             About us <ArrowTopRightIcon />
           </Button>
         </div>
-        <p className="col-span-7 text-4xl leading-normal font-light text-balance">
+        <p className="pointer-events-none z-10 col-span-7 text-4xl leading-normal font-light text-balance">
           Whether you&apos;re a seasoned investor or new to real estate, Piptan
           Investment delivers seamless capital{" "}
           <span className="font-medium">
@@ -44,6 +46,48 @@ export default function Home() {
           </span>{" "}
           Empowering you to invest with confidence.
         </p>
+        <div className="absolute inset-x-0 inset-y-0">
+          <MomentumLines />
+        </div>
+      </section>
+      <section className="container py-24">
+        <p className="mb-3">Our Expertise</p>
+        <div className="grid grid-cols-3 gap-6">
+          <h2 className="col-span-2 text-5xl font-bold">
+            Real Estate Wisdom. <br />
+            Built Over Time.
+          </h2>
+          <p className="text-xl font-light text-balance">
+            With deep market knowledge and strategic insights, we help you make
+            the right move — every time.
+          </p>
+        </div>
+
+        <Button>Button</Button>
+
+        <button
+          className={cn(
+            // Base styles
+            "font-inter group relative w-fit cursor-pointer overflow-hidden rounded-full border border-current px-6 py-2 text-lg font-medium tracking-wide text-white",
+            // Effect styles
+            "transition-transform duration-700 ease-out will-change-transform hover:scale-x-[1.02] hover:ease-[cubic-bezier(.34,5.56,.64,1)]"
+          )}
+        >
+          <span
+            data-text={"Invert Button"}
+            className={cn(
+              "relative block overflow-hidden",
+              // After child
+              "after:absolute after:left-0 after:z-1 after:translate-y-full after:transform after:text-black after:duration-700 after:ease-[cubic-bezier(.4,0,0,1)] after:will-change-transform after:content-[attr(data-text)] group-hover:after:translate-y-0"
+            )}
+          >
+            <span className="inline-block duration-700 ease-[cubic-bezier(.4,0,0,1)] group-hover:-translate-y-full">
+              Button
+            </span>
+          </span>
+
+          <span className="absolute inset-0 translate-y-full rounded-[50%_50%_0_0] bg-white transition-all duration-500 ease-[cubic-bezier(.4,0,0,1)] group-hover:translate-y-0 group-hover:rounded-none"></span>
+        </button>
       </section>
     </main>
   );
---
+++ src/components/animation/momentum-lines.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { MouseEvent, useRef } from "react";
+
+import gsap from "gsap";
+
+const THRESHOLD = 200;
+const ARRAY_LENGTH = 17;
+
+export default function MomentumLines() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const transformX = (el: HTMLDivElement, value: number) => {
+    const xTo = gsap.quickTo(el, "x", {
+      duration: 0.8,
+      ease: "power3.out",
+    });
+
+    xTo(value);
+  };
+
+  const toggleOpacity = (el: HTMLDivElement, value: number) => {
+    const opacityTo = gsap.quickTo(el, "opacity", {
+      duration: 0.8,
+      ease: "power3.out",
+    });
+
+    opacityTo(value);
+  };
+
+  const followMouse = (mouseX: number) => {
+    if (!containerRef.current) return;
+    const lines = containerRef.current?.querySelectorAll(".line");
+    const containerWidth = containerRef.current.offsetWidth;
+
+    lines.forEach((line, index) => {
+      const lineDiv = line as HTMLDivElement;
+
+      const relativeLeft =
+        lineDiv.offsetLeft - containerRef.current!.offsetLeft;
+
+      const xPercentage = mouseX / containerWidth;
+      const linePercentage = relativeLeft / containerWidth;
+      const gapPercentage = Math.abs(xPercentage - linePercentage);
+      const isLastOrFirst = !index || index === lines.length - 1;
+      const threshold = isLastOrFirst ? THRESHOLD / 2 : THRESHOLD;
+
+      const gapDiff = Math.min(
+        Math.max(mouseX - relativeLeft, -threshold),
+        threshold
+      );
+
+      transformX(lineDiv, gapDiff * (1 - gapPercentage));
+      toggleOpacity(lineDiv, 1 - gapPercentage);
+    });
+  };
+
+  const onMouseMove = (e: MouseEvent) => {
+    const mouseX = e.nativeEvent.offsetX;
+    followMouse(mouseX);
+  };
+
+  const onMouseOut = () => {
+    const lines = containerRef.current?.querySelectorAll(".line");
+
+    lines?.forEach((line) => {
+      transformX(line as HTMLDivElement, 0);
+      toggleOpacity(line as HTMLDivElement, 1);
+    });
+  };
+  return (
+    <div
+      onMouseMove={onMouseMove}
+      onMouseOut={onMouseOut}
+      className="flex h-full w-full justify-between p-10"
+      ref={containerRef}
+    >
+      {[...new Array(ARRAY_LENGTH).fill(0)].map((_, index) => {
+        return (
+          <div
+            key={index}
+            className="line via-muted pointer-events-none h-full w-px bg-gradient-to-b from-transparent to-transparent"
+          ></div>
+        );
+      })}
+    </div>
+  );
+}
