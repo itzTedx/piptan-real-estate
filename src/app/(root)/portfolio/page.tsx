a19cb2f49446d65fb98bf040a96463ea37915ae0
melwin <114172287+itzTedx@users.noreply.github.com>
2025-07-12 14:40:49 +0400

    Refactor ProjectsPage to streamline project listing and enhance user experience
    - Removed unused components and pagination logic from `ProjectsPage` for a cleaner layout.
    - Introduced `PortfolioList` component to handle project rendering, improving code organization and readability.
    - Simplified the rendering logic to focus on essential elements, enhancing overall performance.

--- src/app/(root)/portfolio/page.tsx
+++ src/app/(root)/portfolio/page.tsx
@@ -3,27 +3,15 @@ import { Suspense } from "react";
 
 import { SearchParams } from "nuqs";
 
-import { AnimatedGroup } from "@/components/animation/animated-group";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
 import { SectionHeader } from "@/components/ui/section-header";
 import { Separator } from "@/components/ui/separator";
 import { LeadSection } from "@/features/forms/lead-form/section";
 import { getPaginatedProjects } from "@/features/projects/actions/projects-actions";
 import { loadSearchParams } from "@/features/projects/search-params";
-import { PortfolioEmptyState } from "@/features/properties/components/portfolio-empty-state";
 import { PortfolioFilters } from "@/features/properties/components/portfolio-filters";
+import { PortfolioList } from "@/features/properties/components/portfolio-list";
 import { PropertiesListSkeleton } from "@/features/properties/components/properties-list-skeleton";
-import { PropertyCard } from "@/features/properties/components/property-card";
 import { getCategories } from "@/lib/sanity/fetch";
-import { cn, generatePagination } from "@/lib/utils";
 
 type PageProps = {
   searchParams: Promise<SearchParams>;
@@ -51,8 +39,6 @@ export default async function ProjectsPage({ searchParams }: PageProps) {
     searchQuery,
     category
   );
-  const totalPages = Math.ceil(total / pageSize);
-  const pagination = generatePagination(page, totalPages);
 
   return (
     <main className="pt-4 sm:pt-9 md:pt-12">
@@ -64,74 +50,18 @@ export default async function ProjectsPage({ searchParams }: PageProps) {
         <Separator />
 
         {/* Filters */}
-
         <PortfolioFilters categories={categories} />
 
         <Suspense fallback={<PropertiesListSkeleton />}>
-          {projects.length === 0 ? (
-            <PortfolioEmptyState className="my-8" />
-          ) : (
-            <AnimatedGroup
-              preset="blur-slide"
-              className={cn(
-                "grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2"
-              )}
-            >
-              {projects.map((project) => (
-                <PropertyCard
-                  key={project._id}
-                  data={project}
-                  className="max-sm:py-6"
-                />
-              ))}
-            </AnimatedGroup>
-          )}
+          <PortfolioList
+            projects={projects}
+            total={total}
+            page={page}
+            pageSize={pageSize}
+            searchQuery={searchQuery}
+            category={category}
+          />
         </Suspense>
-
-        {totalPages > 1 && (
-          <Pagination className="mt-12">
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious
-                  href={
-                    page > 1
-                      ? `?page=${page - 1}${searchQuery ? `&q=${searchQuery}` : ""}${category && category !== "all" ? `&category=${category}` : ""}`
-                      : "#"
-                  }
-                  className={page <= 1 ? "pointer-events-none opacity-50" : ""}
-                />
-              </PaginationItem>
-
-              {pagination.map((pageNumber, index) => (
-                <PaginationItem key={index}>
-                  {pageNumber === "..." ? (
-                    <PaginationEllipsis />
-                  ) : (
-                    <PaginationLink
-                      href={`?page=${pageNumber}${searchQuery ? `&q=${searchQuery}` : ""}${category && category !== "all" ? `&category=${category}` : ""}`}
-                      isActive={pageNumber === page}
-                    >
-                      {pageNumber}
-                    </PaginationLink>
-                  )}
-                </PaginationItem>
-              ))}
-
-              <PaginationItem>
-                <PaginationNext
-                  href={
-                    page < totalPages
-                      ? `?page=${page + 1}${searchQuery ? `&q=${searchQuery}` : ""}${category && category !== "all" ? `&category=${category}` : ""}`
-                      : "#"
-                  }
-                  className={
-                    page >= totalPages ? "pointer-events-none opacity-50" : ""
-                  }
-                />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
-        )}
       </section>
       <LeadSection
         title={`Let the experts help you\nmake the right investment`}
