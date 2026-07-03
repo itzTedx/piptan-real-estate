6283056c4b01e2b007a7c72ebabcb6bcdf95461d
zironpro <dev@zironpro.ae>
2026-07-03 12:21:00 +0530

    feat: updated the google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,10 @@ export const metadata: Metadata = {
 	},
 	manifest: "/manifest.json",
 	verification: {
-		google: "4jnLKLwAQxiAxdxZcSMWXA3NKw3f79_By4a_AlLNCPQ",
+		google: [
+			"4jnLKLwAQxiAxdxZcSMWXA3NKw3f79_By4a_AlLNCPQ",
+			"haQWE4Rdy3_xxnLrXv87QDJ-XgAUwlCJRVg3VDT0dF0",
+		],
 	},
 	robots: {
 		index: true,
